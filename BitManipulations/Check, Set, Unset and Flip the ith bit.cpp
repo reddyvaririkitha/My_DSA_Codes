cc38706b155ed67b1c8435bdf3fc43db1cd6836d
Reddyvari Rikitha <73999899+reddyvaririkitha@users.noreply.github.com>
2021-08-11 19:47:26 +0530

    Update and rename Check, Set and unset the ith bit.cpp to Check, Set, Unset and Flip the ith bit.cpp

--- BitManipulations/Check, Set, Unset and Flip the ith bit.cpp
+++ BitManipulations/Check, Set, Unset and Flip the ith bit.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void flipAroundIthBit(int n, int i){
+    cout<<"After flipping around ith bit, ans = "<< (n ^ (1<<i))<<endl; //only that bit of the number would be changed.
+
 void turnOffIthBit(int n, int i){
-    cout<<"after turning off the bit, ans = "<< (n&~(1<<i)) <<endl;
+    cout<<"after turning off the bit, ans = "<< (n&~(1<<i)) <<endl; //ith bit would be turned off
 }
 
 
 void turnOnIthBit(int n, int i){
     int ans = (n | (1<<i));
-    cout <<"after turning on the bit, ans = "<<ans<<endl;
+    cout <<"after turning on the bit, ans = "<<ans<<endl; //ith bit would be turned on
 }
 
 void checkithebit(int n, int i){
@@ -24,10 +27,11 @@ int main(){
    cout<<"1.Check if ith bit is set or not."<<endl;
    cout<<"2.Set the ith bit of the number."<<endl;
    cout<<"3.Unset the ith bit of the number."<<endl;
+   cout<<"4.Flip the number around the ith bit."<<endl;
    cout<<"Enter the number and the ith bit: ";
    int n,i,option;
    cin>>n>>i;
-   cout<<"Select the option-1,2,3: ";
+   cout<<"Select the option-1,2,3,4: ";
    cin>>option;
    switch(option){
     case 1:
@@ -39,6 +43,10 @@ int main(){
     case 3:
         turnOffIthBit(n,i);
         break;
+    case 4:
+        flipAroundIthBit(n,i);
+        break;
+        
    }
    
     
