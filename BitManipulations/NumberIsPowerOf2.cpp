64798b5f064c5fdad3d65470111f7290efd83bfa
Reddyvari Rikitha <73999899+reddyvaririkitha@users.noreply.github.com>
2021-08-11 20:09:38 +0530

    Update NumberIsPowerOf2.cpp

--- BitManipulations/NumberIsPowerOf2.cpp
+++ BitManipulations/NumberIsPowerOf2.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 // This fucntion checks if the given number is a perfect power of 2 or not. (like, 2, 4, 8, 16,etc.)
 void checkIfPowerOf2(int n){
-    int z = n & (n-1);
-    if(z==0){
-        cout<<"number is power of 2"<<endl;
-    }else{
+    if( n==1){
         cout<<"number is not a power of 2"<<endl;
+    }
+    else{
+        int z = n & (n-1);
+        if(z==0){
+            cout<<"number is power of 2"<<endl;
+        }else{
+            cout<<"number is not a power of 2"<<endl;
+        }
     }
 }
 
